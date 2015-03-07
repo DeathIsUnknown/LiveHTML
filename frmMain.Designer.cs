e885d1e1e752bb697469d2f9d6cbf9abc58cafff
DeathIsUnknown <deathisunknown@gmail.com>
2015-03-07 20:08:54 +1000

    Initial Commit

---
+++ frmMain.Designer.cs
@@ -0,0 +1,238 @@
+﻿namespace LiveHTML
+{
+    partial class frmMain
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(frmMain));
+            this.txtHTML = new System.Windows.Forms.RichTextBox();
+            this.webBrowser = new System.Windows.Forms.WebBrowser();
+            this.btnInject = new System.Windows.Forms.Button();
+            this.btnHTML = new System.Windows.Forms.Button();
+            this.btnApply = new System.Windows.Forms.Button();
+            this.btnReload = new System.Windows.Forms.Button();
+            this.splitContainer = new System.Windows.Forms.SplitContainer();
+            this.txtFind = new System.Windows.Forms.TextBox();
+            this.txtScript = new System.Windows.Forms.RichTextBox();
+            this.pnlControls = new System.Windows.Forms.Panel();
+            this.pnlURL = new System.Windows.Forms.Panel();
+            this.txtURL = new System.Windows.Forms.TextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer)).BeginInit();
+            this.splitContainer.Panel1.SuspendLayout();
+            this.splitContainer.Panel2.SuspendLayout();
+            this.splitContainer.SuspendLayout();
+            this.pnlControls.SuspendLayout();
+            this.pnlURL.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // txtHTML
+            // 
+            this.txtHTML.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.txtHTML.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.txtHTML.Location = new System.Drawing.Point(3, 23);
+            this.txtHTML.Name = "txtHTML";
+            this.txtHTML.ReadOnly = true;
+            this.txtHTML.Size = new System.Drawing.Size(371, 230);
+            this.txtHTML.TabIndex = 0;
+            this.txtHTML.Text = "";
+            // 
+            // webBrowser
+            // 
+            this.webBrowser.AllowWebBrowserDrop = false;
+            this.webBrowser.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.webBrowser.Location = new System.Drawing.Point(0, 0);
+            this.webBrowser.MinimumSize = new System.Drawing.Size(20, 20);
+            this.webBrowser.Name = "webBrowser";
+            this.webBrowser.ScriptErrorsSuppressed = true;
+            this.webBrowser.Size = new System.Drawing.Size(380, 537);
+            this.webBrowser.TabIndex = 1;
+            this.webBrowser.Url = new System.Uri("https://www.facebook.com/messages/", System.UriKind.Absolute);
+            this.webBrowser.Navigated += new System.Windows.Forms.WebBrowserNavigatedEventHandler(this.webBrowser_Navigated);
+            // 
+            // btnInject
+            // 
+            this.btnInject.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnInject.Location = new System.Drawing.Point(296, 3);
+            this.btnInject.Name = "btnInject";
+            this.btnInject.Size = new System.Drawing.Size(75, 23);
+            this.btnInject.TabIndex = 2;
+            this.btnInject.Text = "Inject";
+            this.btnInject.UseVisualStyleBackColor = true;
+            this.btnInject.Click += new System.EventHandler(this.btnInject_Click);
+            // 
+            // btnHTML
+            // 
+            this.btnHTML.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.btnHTML.Location = new System.Drawing.Point(3, 3);
+            this.btnHTML.Name = "btnHTML";
+            this.btnHTML.Size = new System.Drawing.Size(75, 23);
+            this.btnHTML.TabIndex = 3;
+            this.btnHTML.Text = "Get HTML";
+            this.btnHTML.UseVisualStyleBackColor = true;
+            this.btnHTML.Click += new System.EventHandler(this.btnHTML_Click);
+            // 
+            // btnApply
+            // 
+            this.btnApply.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnApply.Location = new System.Drawing.Point(218, 3);
+            this.btnApply.Name = "btnApply";
+            this.btnApply.Size = new System.Drawing.Size(75, 23);
+            this.btnApply.TabIndex = 4;
+            this.btnApply.Text = "Apply";
+            this.btnApply.UseVisualStyleBackColor = true;
+            this.btnApply.Click += new System.EventHandler(this.btnApply_Click);
+            // 
+            // btnReload
+            // 
+            this.btnReload.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnReload.Location = new System.Drawing.Point(140, 3);
+            this.btnReload.Name = "btnReload";
+            this.btnReload.Size = new System.Drawing.Size(75, 23);
+            this.btnReload.TabIndex = 5;
+            this.btnReload.Text = "Reload";
+            this.btnReload.UseVisualStyleBackColor = true;
+            this.btnReload.Click += new System.EventHandler(this.btnReload_Click);
+            // 
+            // splitContainer
+            // 
+            this.splitContainer.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.splitContainer.Location = new System.Drawing.Point(0, 24);
+            this.splitContainer.Name = "splitContainer";
+            // 
+            // splitContainer.Panel1
+            // 
+            this.splitContainer.Panel1.Controls.Add(this.txtFind);
+            this.splitContainer.Panel1.Controls.Add(this.txtScript);
+            this.splitContainer.Panel1.Controls.Add(this.txtHTML);
+            this.splitContainer.Panel1.Controls.Add(this.pnlControls);
+            this.splitContainer.Panel1MinSize = 315;
+            // 
+            // splitContainer.Panel2
+            // 
+            this.splitContainer.Panel2.Controls.Add(this.webBrowser);
+            this.splitContainer.Size = new System.Drawing.Size(758, 537);
+            this.splitContainer.SplitterDistance = 374;
+            this.splitContainer.TabIndex = 8;
+            // 
+            // txtFind
+            // 
+            this.txtFind.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.txtFind.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtFind.Location = new System.Drawing.Point(3, 0);
+            this.txtFind.Name = "txtFind";
+            this.txtFind.Size = new System.Drawing.Size(371, 20);
+            this.txtFind.TabIndex = 3;
+            this.txtFind.Text = "Find text...";
+            this.txtFind.Enter += new System.EventHandler(this.txtFind_Enter);
+            this.txtFind.KeyUp += new System.Windows.Forms.KeyEventHandler(this.txtFind_KeyUp);
+            this.txtFind.Leave += new System.EventHandler(this.txtFind_Leave);
+            // 
+            // txtScript
+            // 
+            this.txtScript.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.txtScript.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.txtScript.Location = new System.Drawing.Point(3, 256);
+            this.txtScript.Name = "txtScript";
+            this.txtScript.Size = new System.Drawing.Size(371, 252);
+            this.txtScript.TabIndex = 2;
+            this.txtScript.Text = resources.GetString("txtScript.Text");
+            // 
+            // pnlControls
+            // 
+            this.pnlControls.Controls.Add(this.btnHTML);
+            this.pnlControls.Controls.Add(this.btnInject);
+            this.pnlControls.Controls.Add(this.btnApply);
+            this.pnlControls.Controls.Add(this.btnReload);
+            this.pnlControls.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.pnlControls.Location = new System.Drawing.Point(0, 508);
+            this.pnlControls.Name = "pnlControls";
+            this.pnlControls.Size = new System.Drawing.Size(374, 29);
+            this.pnlControls.TabIndex = 1;
+            // 
+            // pnlURL
+            // 
+            this.pnlURL.Controls.Add(this.txtURL);
+            this.pnlURL.Dock = System.Windows.Forms.DockStyle.Top;
+            this.pnlURL.Location = new System.Drawing.Point(0, 0);
+            this.pnlURL.Name = "pnlURL";
+            this.pnlURL.Size = new System.Drawing.Size(758, 24);
+            this.pnlURL.TabIndex = 9;
+            // 
+            // txtURL
+            // 
+            this.txtURL.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.txtURL.Location = new System.Drawing.Point(3, 2);
+            this.txtURL.Name = "txtURL";
+            this.txtURL.Size = new System.Drawing.Size(752, 20);
+            this.txtURL.TabIndex = 7;
+            this.txtURL.KeyUp += new System.Windows.Forms.KeyEventHandler(this.txtURL_KeyUp);
+            // 
+            // frmMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(758, 561);
+            this.Controls.Add(this.splitContainer);
+            this.Controls.Add(this.pnlURL);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MinimumSize = new System.Drawing.Size(655, 63);
+            this.Name = "frmMain";
+            this.Text = "LiveHTML";
+            this.splitContainer.Panel1.ResumeLayout(false);
+            this.splitContainer.Panel1.PerformLayout();
+            this.splitContainer.Panel2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer)).EndInit();
+            this.splitContainer.ResumeLayout(false);
+            this.pnlControls.ResumeLayout(false);
+            this.pnlURL.ResumeLayout(false);
+            this.pnlURL.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.RichTextBox txtHTML;
+        private System.Windows.Forms.WebBrowser webBrowser;
+        private System.Windows.Forms.Button btnInject;
+        private System.Windows.Forms.Button btnHTML;
+        private System.Windows.Forms.Button btnApply;
+        private System.Windows.Forms.Button btnReload;
+        private System.Windows.Forms.SplitContainer splitContainer;
+        private System.Windows.Forms.Panel pnlControls;
+        private System.Windows.Forms.Panel pnlURL;
+        private System.Windows.Forms.TextBox txtURL;
+        private System.Windows.Forms.RichTextBox txtScript;
+        private System.Windows.Forms.TextBox txtFind;
+    }
+}
+
---
+++ frmMain.cs
@@ -0,0 +1,106 @@
+﻿using mshtml;
+using System;
+using System.IO;
+using System.Windows.Forms;
+
+namespace LiveHTML
+{
+    public partial class frmMain : Form
+    {
+        public frmMain()
+        {
+            InitializeComponent();
+        }
+
+        private void btnInject_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                //Inject a JavaScript <Script></Script> tag in the HEAD of the HTML.
+                HtmlElement headElement = webBrowser.Document.GetElementsByTagName("head")[0];
+                HtmlElement scriptElement = webBrowser.Document.CreateElement("script");
+                IHTMLScriptElement domScriptElement = (IHTMLScriptElement)scriptElement.DomElement;
+                domScriptElement.text = "function applyChanges(){" + txtScript.Text + "}";
+                headElement.AppendChild(scriptElement);
+                Text = "LiveHTML - Successfully Injected";
+            }
+            catch { Text = "LiveHTML - Injection Failed"; }
+        }
+
+        private void btnHTML_Click(object sender, EventArgs e)
+        {
+            //Refresh the current page's HTML in the richTextBox.
+            txtHTML.Text = webBrowser.DocumentText;
+        }
+
+        private void btnApply_Click(object sender, EventArgs e)
+        {
+            //Invoke the newly injected <Script></Script> tag.
+            webBrowser.Document.InvokeScript("applyChanges");
+            Text = "LiveHTML - Injected Script Active";
+        }
+
+        private void btnReload_Click(object sender, EventArgs e)
+        {
+            //Undo the injected <Script></Script> actions.
+            webBrowser.Refresh();
+        }
+
+        private void webBrowser_Navigated(object sender, WebBrowserNavigatedEventArgs e)
+        {
+            //Set the textbox text property to the URL of the current page.
+            txtURL.Text = webBrowser.Url.ToString();
+
+            //Print the current page's HTML to the richTextBox.
+            txtHTML.Text = webBrowser.DocumentText;
+
+            //Change the window's text to match the new page.
+            Text = "LiveHTML - " + webBrowser.DocumentTitle;
+        }
+
+        private void txtURL_KeyUp(object sender, KeyEventArgs e)
+        {
+            //When a new URL is entered into the navbar, navigate to new URL.
+            if (e.KeyCode == Keys.Return || e.KeyCode == Keys.Enter)
+            {
+                webBrowser.Navigate(txtURL.Text);
+            }
+        }
+
+        private void txtFind_KeyUp(object sender, KeyEventArgs e)
+        {
+            //When a new URL is entered into the navbar, navigate to new URL.
+            if (e.KeyCode == Keys.Return || e.KeyCode == Keys.Enter)
+            {
+                if (txtFind.Text.Length > 0)
+                {
+                    int index = -1;
+                    int searchStart = 0;
+                    while ((index = txtHTML.Find(txtFind.Text, searchStart, RichTextBoxFinds.None)) > -1)
+                    {
+                        txtHTML.Select(index, txtFind.Text.Length);
+                        txtHTML.SelectionBackColor = System.Drawing.Color.Yellow;
+                        searchStart = index + txtFind.Text.Length;
+                    }
+                }
+            }
+        }
+
+        private void txtFind_Enter(object sender, EventArgs e)
+        {
+            if (txtFind.Text == "Find text...") {
+                txtFind.Text = "";
+                txtFind.Font = new System.Drawing.Font(txtFind.Font, System.Drawing.FontStyle.Regular);
+            }
+        }
+
+        private void txtFind_Leave(object sender, EventArgs e)
+        {
+            if (txtFind.Text == "")
+            {
+                txtFind.Text = "Find text...";
+                txtFind.Font = new System.Drawing.Font(txtFind.Font, System.Drawing.FontStyle.Italic);
+            }
+        }
+    }
+}
